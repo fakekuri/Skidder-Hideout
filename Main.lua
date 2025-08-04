16b7254a93da73732fe3c2e34c8e4152693d5eb1
it!kuri <dungdepzai2013@gmail.com>
2025-08-04 17:10:20 +0700

    Update Main.lua

--- Main.lua
+++
@@ -1,8 +0,0 @@
-local placeId = game.PlaceId
-if placeId == 116495829188952 then
-    --DeadRails
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/fakekuri/NightXHUB/refs/heads/main/NightXDeadRails.lua"))()
-elseif placeId == 109983668079237 then -- Added 'then' here
-    --Stealabrainrot
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/fakekuri/NightXHUB/refs/heads/main/NightXSAB"))()
-end
