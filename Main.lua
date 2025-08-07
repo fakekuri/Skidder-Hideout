9f9de1cc87a05f52eef896afa3ca8118c8536776
it!kuri <dungdepzai2013@gmail.com>
2025-08-07 11:28:18 +0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,7 @@
-game.Players.LocalPlayer:Kick("Down join https://discord.gg/A7PDRqHMVD for new")
+local placeId = game.PlaceId
+if placeId == 2753915549 or placeId == 4442272183 or placeId == 7449423635 then
+	-- Blox Fruits
+	game.Players.LocalPlayer:Kick("Soon join https://discord.gg/A7PDRqHMVD for new")
+elseif placeId == 79546208627805 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/fakekuri/SeramicHub/main/99nights.lua"))()
+end
